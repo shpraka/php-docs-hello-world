fafca987502763a393b0d9b468b877ca0afd7e28
shpraka <46188819+shpraka@users.noreply.github.com>
2023-10-19 17:20:38 +0530

    removed file1.php

--- file1.php
+++
@@ -1,5 +0,0 @@
-<?php
-function getMessageFromFile1() {
-    return "This message is from file1.php";
-}
-?>
--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <title>PHP Sample Application</title>
 </head>
 <body>
-    <h1>Welcome to my PHP Application test</h1>
+    <h1>Welcome to my PHP Application test. Removed file1.php</h1>
 
     <?php
-    include 'file1.php'; // Include the first PHP file
-    echo "<p>" . getMessageFromFile1() . "</p>";
+    echo "<p>" .. "</p>";
     ?>
 </body>
 </html>
