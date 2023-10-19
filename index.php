9c437605e034c81e2c1f058b6f6e9ec7b2af3ddb
shpraka <46188819+shpraka@users.noreply.github.com>
2023-10-19 17:10:01 +0530

    changed include to require

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to my PHP Application</h1>
 
     <?php
-    include 'file1.php', 'file2.php';
+    require 'file1.php', 'file2.php';
     
     echo "<p>" . getMessageFromFile1() . "</p>";
     echo "<p>" . getMessageFromFile2() . "</p>";
